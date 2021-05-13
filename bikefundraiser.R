ea69549b75e3c477b816ce825bdb8cdddb8e36ae
Eric Simms <esimms@yahoo.com>
2021-05-13 13:42:27 +0000

    Added annotation of total km. Added anim_save.

--- bikefundraiser.R
+++ bikefundraiser.R
@@ -17,20 +17,22 @@ library(png)
 # Import data
 date_cumulativekm <- read_csv("data/date_cumulativekm.csv")
 
-# Derive km per day
+# Derive km per day and a character value for cumulative km
 date_cumulativekm_dailykm <- date_cumulativekm %>%
-  mutate(DAILY_KM = ifelse(is.na(lag(CUMULATIVE_KM)), 0, CUMULATIVE_KM - lag(CUMULATIVE_KM)))
+  mutate(DAILY_KM = ifelse(is.na(lag(CUMULATIVE_KM)), 0, CUMULATIVE_KM - lag(CUMULATIVE_KM))) %>%
+  mutate(CUMULATIVE_KM_CHAR = ifelse(CUMULATIVE_KM < 30, "", as.character(CUMULATIVE_KM)))
 
 # Create series of graphs to be animated
 theme_set(theme_classic())
 animate <- ggplot(data = date_cumulativekm_dailykm, aes(x = DAY, y = CUMULATIVE_KM)) +
   geom_col(aes(group=seq_along(DAY), y = DAILY_KM, color = "red"), width = 0.3, show.legend = FALSE) +
   geom_line(aes(color = "green"), size = 2, show.legend = FALSE) +
+  geom_point() +
+  geom_text(aes(label=CUMULATIVE_KM, vjust= -2), show.legend = FALSE) +
   geom_vline(xintercept = 31, linetype = "dashed") +
   geom_hline(yintercept = 1000, linetype = "dashed") +
-  xlim(0, 31) +
-  ylim(0, 1000) +
-  expand_limits(x = 0, y = 0) +
+  geom_abline(slope = 1000/31, linetype = "dotted") +
+  coord_cartesian(xlim = c(0,31), ylim = c(0, 1000), expand = FALSE) +
   transition_reveal(DAY) +
   labs(title = "Daily and Cumulative Km by Challenge Day",
        subtitle = "Day: {floor(frame_along)}",
@@ -38,3 +40,4 @@ animate <- ggplot(data = date_cumulativekm_dailykm, aes(x = DAY, y = CUMULATIVE_
 
 # Create animated gif
 animate(animate, renderer=gifski_renderer(), nframes = 20, duration = 20, fps = 10, end_pause = 60, rewind = FALSE)
+anim_save("fileec72091c1a.gif", path = "output")
